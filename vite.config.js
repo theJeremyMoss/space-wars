f7d79299780c00effe05511c93fddad7c4a31489
theJeremyMoss <git@ffio.net>
2025-11-22 15:58:24 -0800

    fix deploy path issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './', // Relative base path for GitHub Pages
+    base: '/space-wars/', // GitHub Pages base path (repo name)
     build: {
         assetsInlineLimit: 0, // Ensure assets are not inlined as base64
     },
     server: {
         host: true
     }
-});
+}); 
